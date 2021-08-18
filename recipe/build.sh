61bb1be06f82c4239f8e30ad310dc917883be106
Kai Tietz <ktietz@anaconda.com>
2021-08-18 10:01:13 +0200

    add osx-arm64 support, and disabled test due URL has invalid certificate

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [[ $(uname) == Darwin ]]; then
+    export LDFLAGS="-headerpad_max_install_names $LDFLAGS"
+fi
+
 export netCDF4_DIR=$PREFIX
 export HDF5_DIR=$PREFIX
 
--- recipe/run_test.py
+++ recipe/run_test.py
@@ -4,11 +4,12 @@ import netCDF4
 import os
 os.environ['TEMP'] = os.environ['PREFIX']
 
+## disabled due invalid certificate
 # OPeNDAP.
-url = 'http://geoport-dev.whoi.edu/thredds/dodsC/estofs/atlantic'
-with netCDF4.Dataset(url) as nc:
-    # Compiled with cython.
-    assert nc.filepath() == url
+#url = 'http://geoport-dev.whoi.edu/thredds/dodsC/estofs/atlantic'
+#with netCDF4.Dataset(url) as nc:
+#    # Compiled with cython.
+#    assert nc.filepath() == url
 
 
 url = 'http://geoport.whoi.edu/thredds/dodsC/usgs/vault0/models/tides/vdatum_gulf_of_maine/adcirc54_38_orig.nc'
